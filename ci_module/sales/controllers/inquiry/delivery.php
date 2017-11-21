4a7f4b44407acf0baae88bad37d5e1cb99805925
Reyhan Fikran Dz <31202910+zeetl@users.noreply.github.com>
2017-11-21 14:18:28 +0700

    Up
    menu

--- ci_module/sales/controllers/inquiry/delivery.php
+++ ci_module/sales/controllers/inquiry/delivery.php
@@ -25,33 +25,34 @@ class SalesInquiryDelivery
     private function filter()
     {
         row_start();
-        col_start(3);
+        col_start(8,"col-md-8 col-md-offset-2");
         input_text('#','DeliveryNumber',null,true);
 //         ref_cells(_("#:"), 'DeliveryNumber', '', null, '', true);
 
-        col_start(3);
+        // col_start(3);
         input_date_bootstrap('from', 'DeliveryAfterDate',null,false, -30);
 
-        col_start(3);
+        // col_start(3);
         input_date_bootstrap('to', 'DeliveryToDate',null,false,1);
 
-        col_start(3);
+        // col_start(3);
         locations_bootstrap('Location', 'StockLocation',null,true);
 
-        row_start();
-        col_start(4);
+        // row_start();
+        // col_start(4);
         stock_items_bootstrap('Item', 'SelectStockFromList', null, true);
 //         stock_items_list_cells(_("Item:"), 'SelectStockFromList', null, true);
 
-        col_start(4);
+        // col_start(4);
         customer_list_bootstrap(_("Customer"), 'customer_id', null, true);
 //         customer_list_cells(_("Select a customer: "), 'customer_id', null, true, true);
 
-        col_start(1);
+        // col_start(1);
         submit_bootstrap('SearchOrders', _("Search"), _('Select documents'), 'default' , 'search');
 //         submit('SearchOrders', _("Search"), true , _('Select documents'), 'default');
 
         hidden('OutstandingOnly', $_POST['OutstandingOnly']);
+        
         row_end();
 
     }
